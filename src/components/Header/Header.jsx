a1e22f2b1d53cbffae2ad9fe9211dadeb7c94299
Sahand-feizi <sahandfeizi87@gmail.com>
2025-01-15 14:07:06 +0330

    header ui

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+const headerData = [
+    {
+        id: 1,
+        url: '/',
+        name: 'Home'
+    },
+    {
+        id: 2,
+        url: '/',
+        name: 'Dashboard'
+    },
+    {
+        id: 3,
+        url: '/',
+        name: 'Login'
+    },
+    {
+        id: 4,
+        url: '/',
+        name: 'Sinup'
+    },
+]
+
+function Header() {
+    const [active, setActive] = useState()
+
+    return (
+        <div className='header'>
+            {
+                headerData.map(item => (
+                    <Link
+                        key={item.id}
+                        to={item.url}
+                        className='headerLinkItem'
+                        onClick={(e) => {
+                            e.preventDefault()
+                            setActive(item.id)
+                        }} 
+                    >
+                        <p className={item.id == active && 'active'}>{item.name}</p>
+                        <span className={`indecator ${item.id == active && 'active'}`}></span>
+                    </Link>
+                ))
+            }
+        </div>
+    )
+}
+
+export default Header
